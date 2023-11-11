3b8388f1521ec9395936f7a18db4a421400107d8
Valeriy Lvov <tepliy_shov@mail.ru>
2023-11-11 20:46:16 +0400

    сделал обработку ошибок в регистрации/авторизации

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -29,6 +29,7 @@ def register(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы успешно зарегестрировались!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
