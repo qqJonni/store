74265d42e52453bc70a7ff3fc2225279957a0aed
Valeriy Lvov <tepliy_shov@mail.ru>
2023-11-11 12:45:36 +0400

    добавил вёрстку для user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    context = {'title': 'Store - Авторизация'}
+    return render(request, 'users/login.html', context)
+
+
+def register(request):
+    context = {'title': 'Store - Регистрация'}
+    return render(request, 'users/register.html', context)
